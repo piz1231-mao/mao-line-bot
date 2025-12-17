4251af5ba9b2cdf64f7f06c51608f353507eb65b
piz1231-mao <piz1231@gmail.com>
2025-12-18 00:31:35 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,21 +1,15 @@
 module.exports = {
   keywords: ["help", "指令", "說明"],
   handler: async (client, event) => {
-    const text =
+    await client.replyMessage(event.replyToken, {
+      type: "text",
+      text:
 `📖 毛怪秘書指令表
 ━━━━━━━━━━━
 📌 查ID / 群組ID
 📝 待辦：事項
-📊 業績：店名 / 金額
-🧹 特清：內容
-📣 客怨：摘要
 🧪 流檢：結果
-📝 記事：內容
-`;
-
-    await client.replyMessage(event.replyToken, {
-      type: "text",
-      text
+📣 客怨：摘要`
     });
   }
 };
