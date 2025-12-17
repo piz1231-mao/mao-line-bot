c0daf2f1c2f641e1ef4249aca68f50ca139ee5a8
piz1231-mao <piz1231@gmail.com>
2025-12-18 01:02:25 +0800

    Update id.js

--- commands/id.js
+++ commands/id.js
@@ -1,15 +1,1 @@
-module.exports = {
-  keywords: ["查id", "我的id", "群組id"],
-  handler: async (client, event) => {
-    const s = event.source;
-    const text =
-      s.type === "group" ? `📌 群組 ID：${s.groupId}` :
-      s.type === "room"  ? `📌 聊天室 ID：${s.roomId}` :
-                           `📌 User ID：${s.userId}`;
 
-    await client.replyMessage(event.replyToken, {
-      type: "text",
-      text
-    });
-  }
-};
