fe3bc87584682da685983fd6a9361eb66383c101
piz1231-mao <piz1231@gmail.com>
2025-12-23 20:24:59 +0800

    Create sessionKey.js

---
+++ utils/sessionKey.js
@@ -0,0 +1,15 @@
+// utils/sessionKey.js
+function getSessionKey(event) {
+  const userId = event.source.userId;
+
+  const sourceId =
+    event.source.type === "group"
+      ? event.source.groupId
+      : event.source.type === "room"
+      ? event.source.roomId
+      : event.source.userId; // 私訊
+
+  return `${userId}:${sourceId}`;
+}
+
+module.exports = { getSessionKey };
