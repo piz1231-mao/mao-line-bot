0b230c02a4ce5e016a25d868597be504084ead27
piz1231-mao <piz1231@gmail.com>
2025-12-23 20:37:27 +0800

    Update sessionKey.js

--- utils/sessionKey.js
+++ utils/sessionKey.js
@@ -1,13 +1,12 @@
 // utils/sessionKey.js
 function getSessionKey(event) {
   const userId = event.source.userId;
-
   const sourceId =
     event.source.type === "group"
       ? event.source.groupId
       : event.source.type === "room"
       ? event.source.roomId
-      : event.source.userId; // 私訊
+      : event.source.userId;
 
   return `${userId}:${sourceId}`;
 }
