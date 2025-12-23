483c3d52d7056580ca9de8283f36fe828dadcb98
piz1231-mao <piz1231@gmail.com>
2025-12-23 19:01:05 +0800

    Create hsrStations.js

---
+++ utils/hsrStations.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "南港": "THSR_Nangang",
+  "台北": "THSR_Taipei",
+  "板橋": "THSR_Banqiao",
+  "桃園": "THSR_Taoyuan",
+  "新竹": "THSR_Hsinchu",
+  "苗栗": "THSR_Miaoli",
+  "台中": "THSR_Taichung",
+  "彰化": "THSR_Changhua",
+  "雲林": "THSR_Yunlin",
+  "嘉義": "THSR_Chiayi",
+  "台南": "THSR_Tainan",
+  "左營": "THSR_Zuoying"
+};
